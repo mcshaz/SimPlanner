e87814ce662f667b100b71fb0eb0b0513fbb119d
Brent McSharry <brentm@adhb.govt.nz>
2016-10-06 00:29:55 +1300

    course appointment errors

--- SP.Web/UserEmails/CreateCourseAppointment.cs
+++ SP.Web/UserEmails/CreateCourseAppointment.cs
@@ -1,5 +1,4 @@
-﻿using HtmlAgilityPack;
-using SP.DataAccess;
+﻿using SP.DataAccess;
 using System;
 using System.IO;
 using System.Linq;
@@ -10,27 +9,45 @@ using System.Text;
 using Ical.Net;
 using Ical.Net.DataTypes;
 using Ical.Net.Serialization.iCalendar.Serializers;
-using Ical.Net.General;
 using Ical.Net.Interfaces.DataTypes;
+using System.Collections.Generic;
 
 namespace SP.Web.UserEmails
 {
     public static class Appointment
     {
-        const string mailto = "MAILTO:";
-        static public AppointmentStream CreateiCalendar(Course course, IIdentity currentUser)
+        private static Calendar CreateCal(Course course, out TimeZoneInfo tzi)
         {
-            var currentCal = new Calendar {
-                Method = course.Cancelled ? CalendarMethods.Cancel: CalendarMethods.Publish,
+            var currentCal = new Calendar
+            {
+                Method = course.Cancelled ? CalendarMethods.Cancel : CalendarMethods.Request,
             };
-            var tzi = TimeZoneInfo.FindSystemTimeZoneById(course.Department.Institution.StandardTimeZone);
+            tzi = TimeZoneInfo.FindSystemTimeZoneById(course.Department.Institution.StandardTimeZone);
             var timezone = VTimeZone.FromSystemTimeZone(tzi);
             currentCal.AddTimeZone(timezone);
+            return currentCal;
+        }
+        private static GeographicLocation GetGeoLocation(Course course)
+        {
+            if (course.Department.Institution.Latitude.HasValue && course.Department.Institution.Longitude.HasValue)
+            {
+                return new GeographicLocation(course.Department.Institution.Latitude.Value, course.Department.Institution.Longitude.Value);
+            }
+            return null;
+        }
+        private static Organizer GetOrganizer(Course course)
+        {
+            const string SimPlannerInfo = mailto + "info@sim-planner.com"; //ToDo read from web.config mail settings
+            return new Organizer() { Value = new Uri(SimPlannerInfo), CommonName = "sim-planner.com" };
+        }
 
+        const string mailto = "mailto:";
+        static public Calendar CreateCourseAppointment(Course course, IIdentity currentUser)
+        {
+            TimeZoneInfo tzi;
+            var currentCal = CreateCal(course, out tzi);
             // Create the event, and add it to the iCalendar
             //
-            const string SimPlannerInfo = mailto + "info@SimPlanner.org"; //ToDo read from web.config mail settings
-
             Event courseEvt = new Event
             {
                 Class = "PRIVATE",
@@ -38,16 +55,18 @@ namespace SP.Web.UserEmails
                 LastModified = new CalDateTime(course.LastModifiedUtc),
                 Sequence = course.EmailSequence,
                 Transparency = TransparencyType.Opaque,
-                Status = course.Cancelled? EventStatus.Cancelled:EventStatus.Confirmed,
+                Status = course.Cancelled ? EventStatus.Cancelled : EventStatus.Confirmed,
                 Uid = "course" + course.Id.ToString(),
                 Priority = 5,
                 Location = course.Room.ShortDescription,
-                Organizer = new Organizer(SimPlannerInfo) { CommonName = "SimPlanner" },
                 Attendees = course.CourseParticipants.Select(MapCourseParticipantToAttendee).ToList(),
                 Summary = course.Department.Abbreviation + " " + course.CourseFormat.CourseType.Abbreviation,
                 IsAllDay = false,
-                Description = course.Department.Name + " " + course.CourseFormat.CourseType.Description
+                Description = course.Department.Name + " " + course.CourseFormat.CourseType.Description,
+                GeographicLocation = GetGeoLocation(course),
+                Organizer = GetOrganizer(course)
             };
+            System.Diagnostics.Debug.WriteLine(courseEvt.Organizer);
             foreach (var cd in course.AllDays().Take(course.CourseFormat.DaysDuration))
             {
                 var start = TimeZoneInfo.ConvertTimeFromUtc(cd.StartUtc, tzi);
@@ -65,12 +84,6 @@ namespace SP.Web.UserEmails
             }
 
 
-            
-
-            if (course.Department.Institution.Latitude.HasValue && course.Department.Institution.Longitude.HasValue)
-            {
-                courseEvt.GeographicLocation = new GeographicLocation(course.Department.Institution.Latitude.Value, course.Department.Institution.Longitude.Value);
-            }
 
             // Create a serialization context and serializer factory.
             // These will be used to build the serializer for our object.
@@ -82,21 +95,23 @@ namespace SP.Web.UserEmails
                 Summary = course.Department.Abbreviation + ' ' + course.CourseFormat.CourseType.Abbreviation,
                 Trigger = new Trigger(TimeSpan.FromHours(-1))
             };
-            
+
             // Add the alarm to the event
             courseEvt.Alarms.Add(alarm);
 
-            return new AppointmentStream(currentCal);
+            return currentCal;
         }
+
 
         private static IAttendee MapCourseParticipantToAttendee(CourseParticipant cp)
         {
             string email = cp.Participant.Email;
             if (!string.IsNullOrEmpty(cp.Participant.AlternateEmail))
             {
-                email += ',' + cp.Participant.AlternateEmail;
+                email += "?cc=" + cp.Participant.AlternateEmail;
             }
-            return new Attendee(mailto + email)
+            var uri = new Uri(mailto + email);
+            return new Attendee(uri)
             {
                 CommonName = cp.Participant.FullName,
                 Role = "REQ-PARTICIPANT",
@@ -109,30 +124,44 @@ namespace SP.Web.UserEmails
             };
         }
 
-        public static void AddFacultyMeeting(Calendar cal, Course course)
+        public static Calendar CreateFacultyCalendar(Course course)
+        {
+            TimeZoneInfo tzi;
+            var currentCal = CreateCal(course, out tzi);
+            var facultyMeet = CreateFacultyMeetingEvent(course, tzi);
+            currentCal.Events.Add(facultyMeet);
+            return currentCal;
+        }
+
+        public static void AppendFacultyMeetingEvent(Calendar cal, Course course)
         {
             if (!course.FacultyMeetingUtc.HasValue) { return; }
             //CalendarMethods.Request is only valid for single event per calendar
-            cal.Method = course.Cancelled ? CalendarMethods.Cancel : CalendarMethods.Publish; 
-
-            var tzi = TimeZoneInfo.FindSystemTimeZoneById(course.Department.Institution.StandardTimeZone);
+            cal.Method = course.Cancelled ? CalendarMethods.Cancel : CalendarMethods.Publish;
             var courseEvent = cal.Events.First();
+            var facultyMeet = CreateFacultyMeetingEvent(course, TimeZoneInfo.FindSystemTimeZoneById(course.Department.Institution.StandardTimeZone));
+            cal.Events.Add(facultyMeet);
+        }
+
+        public static Event CreateFacultyMeetingEvent(Course course, TimeZoneInfo tzi)
+        {
             var start = TimeZoneInfo.ConvertTimeFromUtc(course.StartUtc, tzi);
             Event meeting = new Event
             {
-                Class = "Private",
+                Class = "PRIVATE",
                 Created = new CalDateTime(course.CreatedUtc),
                 LastModified = new CalDateTime(course.LastModifiedUtc),
                 Sequence = course.EmailSequence,
                 Uid = "planning" + course.Id.ToString(),
                 Priority = 5,
+                Organizer = GetOrganizer(course),
                 Transparency = TransparencyType.Opaque,
                 Status = course.Cancelled ? EventStatus.Cancelled : EventStatus.Confirmed,
                 Description = "planning meeting for " + course.Department.Name + " " + course.CourseFormat.CourseType.Description + " - " + start.ToString("g"),
                 Summary = course.Department.Abbreviation + " " + course.CourseFormat.CourseType.Abbreviation + " planning meeting - " + start.ToString("d"),
-                Organizer = courseEvent.Organizer,
                 Attendees = course.CourseParticipants.Where(cp => cp.IsFaculty).Select(MapCourseParticipantToAttendee).ToList(),
-                Start = new CalDateTime(TimeZoneInfo.ConvertTimeFromUtc(course.FacultyMeetingUtc.Value, tzi), tzi.Id)
+                Start = new CalDateTime(TimeZoneInfo.ConvertTimeFromUtc(course.FacultyMeetingUtc.Value, tzi), tzi.Id),
+                GeographicLocation = GetGeoLocation(course)
             };
 
             // Set information about the event
@@ -153,82 +182,110 @@ namespace SP.Web.UserEmails
             // Add the alarm to the event
             meeting.Alarms.Add(alarm);
 
-            cal.Events.Add(meeting);
+            return meeting;
         }
     }
 
     public sealed class AppointmentStream : IDisposable
     {
-        public AppointmentStream(Calendar cal)
+        public AppointmentStream(params Calendar[] cals)
         {
-            Cal = cal;
+            _cals = new List<CalendarInfo>();
             _serializer = new CalendarSerializer();
-            
-
-        }
-        public readonly Calendar Cal;
-        readonly SerializerBase _serializer;
-
-        ContentType _calType;
-        ContentType CalType
-        {
-            get
+            foreach (var c in cals)
             {
-                if (_calType == null)
-                {
-                    _calType = new ContentType("text/calendar");
-                    _calType.CharSet = Encoding.UTF8.HeaderName;
-                    _calType.Name = "appointment.ics";
-                    _calType.Parameters.Add("method", Cal.Method);
-                }
-                return _calType;
+                Add(c);
             }
         }
 
+        public void Add(Calendar cal)
+        {
+            var evt = cal.Events.First();
+            var stream = new MemoryStream();
+            //should work but truncating the stream at the moment - ? flush needed in the ical.net source code
+            //_serializer.Serialize(cal, stream, Encoding.UTF8);
+            //stream.Flush();
+            using (var sw = new StreamWriter(stream, Encoding.UTF8, 2500, true))
+            {
+                sw.Write(_serializer.SerializeToString(cal));
+            }
+                
+            _cals.Add(new CalendarInfo
+            {
+                Name = (evt.Summary ?? evt.Description ?? "Appointment") + ".ics",
+                Data = stream,
+                Method = cal.Method
+            });
+        }
+
+        public void Replace(params Calendar[] cals)
+        {
+            _cals.Clear();
+            foreach (var c in cals)
+            {
+                Add(c);
+            }
+        }
+
+        /// <summary>
+        /// For testing
+        /// </summary>
+        public IEnumerable<Stream> GetStreams()
+        {
+            foreach (var s in _cals)
+            {
+                s.Data.Position = 0;
+                yield return s.Data;
+            }
+        } 
+
+        private readonly IList<CalendarInfo> _cals;
+        private readonly SerializerBase _serializer;
+
+        /*
         private void AddHtmlFromMsg(MailMessage msg)
         {
-            var evt = (Event)Cal.Events.First();
-
-            evt.Description = msg.Body;
-
-            var htmlView = msg.AlternateViews.First(av => av.ContentType.MediaType == MediaTypeNames.Text.Html);
-
-            //remove image tags from doc
-            string html;
-            var htmlDoc = new HtmlDocument();
-            htmlView.ContentStream.Position = 0;
-            using (var sr = new StreamReader(htmlView.ContentStream, Encoding.UTF8, false, 1024, true))
+            foreach (var c in Cals)
             {
-                htmlDoc.Load(sr);
-                foreach (var img in htmlDoc.DocumentNode.SelectNodes("//img"))
+                var evt = (Event)c.Events.First();
+
+                evt.Description = msg.Body;
+
+                var htmlView = msg.AlternateViews.First(av => av.ContentType.MediaType == MediaTypeNames.Text.Html);
+
+                //remove image tags from doc
+                string html;
+                var htmlDoc = new HtmlDocument();
+                htmlView.ContentStream.Position = 0;
+                using (var sr = new StreamReader(htmlView.ContentStream, Encoding.UTF8, false, 1024, true))
                 {
-                    img.RemoveAll();
+                    htmlDoc.Load(sr);
+                    foreach (var img in htmlDoc.DocumentNode.SelectNodes("//img"))
+                    {
+                        img.RemoveAll();
+                    }
+                    html = htmlDoc.DocumentNode.OuterHtml;
                 }
-                html = htmlDoc.DocumentNode.OuterHtml;
+                htmlView.ContentStream.Position = 0;
+                //end remove image tags
+
+                //add html to ical
+                const string altDesc = "X-ALT-DESC";
+                CalendarProperty prop = (CalendarProperty)evt.Properties[altDesc];
+
+                if (prop == null)
+                {
+                    prop = new CalendarProperty(altDesc);
+                    prop.AddParameter("FMTTYPE", MediaTypeNames.Text.Html);
+                    evt.AddProperty(prop);
+                }
+                prop.SetValue(html);
+                //end add HTML to ical
             }
-            htmlView.ContentStream.Position = 0;
-            //end remove image tags
-
-            //add html to ical
-            const string altDesc = "X-ALT-DESC";
-            CalendarProperty prop = (CalendarProperty)evt.Properties[altDesc];
-
-            if (prop == null)
-            {
-                prop = new CalendarProperty(altDesc);
-                prop.AddParameter("FMTTYPE", MediaTypeNames.Text.Html);
-                evt.AddProperty(prop);
-            }
-            prop.SetValue(html);
-            //end add HTML to ical
         }
+        */
 
-        public string IcalString()
-        {
-            return _serializer.SerializeToString(Cal);
-        }
-
-        public void AddAppointmentTo(MailMessage msg)
+        public void AddAppointmentsTo(MailMessage msg)
         {
 
             //writeOutlookFormat
@@ -242,10 +299,21 @@ namespace SP.Web.UserEmails
             */
             //end writeOutlookFormat
 
-            var attach = System.Net.Mail.Attachment.CreateAttachmentFromString(IcalString(), CalType);
+            //hack alert - calendars could conceivably have different methods
+            foreach (var c in _cals)
+            {
+                var contentType = new ContentType("text/calendar")
+                {
+                    CharSet = Encoding.UTF8.HeaderName,
+                    Name = c.Name
+                };
+                contentType.Parameters.Add("method", c.Method);
 
-            msg.Attachments.Add(attach);
+                c.Data.Position = 0;
+                var attach = new System.Net.Mail.Attachment(c.Data, contentType); //System.Net.Mail.Attachment.CreateAttachmentFromString(s, calType);
 
+                msg.Attachments.Add(attach);
+            }
         }
 
         /*
@@ -281,10 +349,21 @@ namespace SP.Web.UserEmails
             }
             if (!_isDisposed)
             {
-                Cal.Dispose();
+                foreach (var c in _cals)
+                {
+                    c.Data.Dispose();
+                }
                 _isDisposed = true;
             }
-            
+
         }
     }
+
+    internal class CalendarInfo
+    {
+        public Stream Data { get; set; }
+        public string Name { get; set; }
+        public string Method { get; set; }
+    }
+
 }
